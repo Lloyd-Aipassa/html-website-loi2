016a2cc267b4b6ae3f63c5dd22354fdd0aa6b695
Lloyd Aipassa <lloyd.aipassa@gmail.com>
2023-10-17 20:18:09 +0200

    new

--- html-website-loi/javascript/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menu = document.querySelector("nav ul");
-
-hamburgerMenu.addEventListener("click", () => {
-menu.classList.toggle("show");
-hamburgerMenu.classList.toggle("open");
-});
--- html-website-loi/javascript/navigatieZichtbaar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    const element =
-        document.querySelector(".page-nav");
-    if (window.scrollY > 1000) {
-        element.classList.add("visible");
-    } else {
-        element.classList.remove("visible");
-    }
-});
